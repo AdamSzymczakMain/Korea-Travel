48a383d6ba5bfaedaa4eb2aa7ee949bdcff513d4
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-10-10 19:14:22 +0200

    add auto swipper

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,43 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay, Pagination } from "swiper/modules";
+import { ChevronDown } from "lucide-react";
+
 const HeroSection = () => {
-  return <div>Hero</div>;
+  const images = [
+    "/img/dmz.png",
+    "/img/jeju.png",
+    "/img/kioto.png",
+    "/img/Palawan.png",
+  ];
+
+  return (
+    <div className="relative h-screen w-full">
+      <Swiper
+        modules={[Autoplay, Pagination]} // Dodanie modułów
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={src}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-full object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default HeroSection;
