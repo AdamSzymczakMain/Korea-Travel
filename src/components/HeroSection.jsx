4c33e953f424c92640be562f5f705e462bd3f5a4
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-10-10 22:20:45 +0200

    add text to hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
   return (
     <div className="relative h-screen w-full overflow-hidden">
       <Swiper
-        modules={[Autoplay, Pagination]} // Dodanie modułów
+        modules={[Autoplay, Pagination]}
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
@@ -36,6 +36,14 @@ const HeroSection = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="absolute inset-0 flex items-center justify-center z-20 pointer-events-none flex-col">
+        <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center px-4 py-2 sm:px-6 sm:py-4 md:px-8 md:py-6 rounded pointer-events-auto">
+          Stwórz najlepsze wspomnienia
+        </h1>
+        <h2 className="text-white text-2xl sm:text-3xl mb-16 md:mb-64 lg:text-5xl xl:text-6xl font-bold text-center px-4 py-2 sm:px-6 sm:py-4 md:px-8 md:py-6 rounded pointer-events-auto">
+          Podróżując z nami
+        </h2>
+      </div>
     </div>
   );
 };
