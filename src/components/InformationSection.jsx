285c0cca97ad28d7e0af4680542d868cc03b8e2f
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-10-12 16:03:17 +0200

    add gradient to text

--- src/components/InformationSection.jsx
+++ src/components/InformationSection.jsx
@@ -5,6 +5,12 @@ const InformationSection = () => {
         <span className="bg-natural-900 text-pink-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
           Informacje
         </span>
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg-mt20 tracking-wide">
+          Łatwy przewodnik
+          <span className="bg-gredient-to-r from-pink-300 to-pink-500 text-transparent bg-clip-text">
+            po stronie
+          </span>
+        </h2>
       </div>
     </div>
   );
