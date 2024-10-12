7889ecf73ff1f69dc55b738413142011ea935e1c
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-10-12 12:51:41 +0200

    add object

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,10 +5,10 @@ import { ChevronDown } from "lucide-react";
 
 const HeroSection = () => {
   const images = [
-    "/img/dmz.png",
-    "/img/jeju.png",
-    "/img/kioto.png",
-    "/img/Palawan.png",
+    { image: "/img/dmz.png", text: "XYZ" },
+    { image: "/img/jeju.png", text: "XYZ" },
+    { image: "/img/kioto.png", text: "XYZ" },
+    { image: "/img/Palawan.png", text: "XYZ" },
   ];
 
   return (
@@ -29,21 +29,14 @@ const HeroSection = () => {
         {images.map((src, index) => (
           <SwiperSlide key={index}>
             <img
-              src={src}
+              src={src.image}
               alt={`Slide ${index + 1}`}
               className="w-full h-full object-cover"
             />
+            <p>{src.text}</p>
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="absolute inset-0 flex items-center justify-center z-20 pointer-events-none flex-col">
-        <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center px-4 py-2 sm:px-6 sm:py-4 md:px-8 md:py-6 rounded pointer-events-auto">
-          Stwórz najlepsze wspomnienia
-        </h1>
-        <h2 className="text-white text-2xl sm:text-3xl mb-16 md:mb-64 lg:text-5xl xl:text-6xl font-bold text-center px-4 py-2 sm:px-6 sm:py-4 md:px-8 md:py-6 rounded pointer-events-auto">
-          Podróżując z nami
-        </h2>
-      </div>
     </div>
   );
 };
