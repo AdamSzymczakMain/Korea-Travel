e1a5996c24b97df4451ea5c59cc2b4ce942dc62a
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-10-15 09:33:28 +0200

    create swiperslide

--- src/components/TripSection.jsx
+++ src/components/TripSection.jsx
@@ -9,14 +9,24 @@ import { itemsTrip } from "../constants";
 const TripSection = () => {
   return (
     <>
-      <div className="flex">
-        <Swiper>
+      <div className="flex overflow-hidden">
+        <Swiper className="w-full h-full">
           {itemsTrip.map((item, index) => (
-            <SwiperSlide key={index}>
-              <div className="m-24 h-28">
-                <img src={item.image} alt="Image ${item.name}" />
-                <h2>{item.name}</h2>
-                <p>{item.description}</p>
+            <SwiperSlide
+              key={index}
+              className="flex flex-col justify-center items-center max-w-60 h-80 my-6 overflow-hidden shadow rounded-xl cursor-grab m-2"
+            >
+              <div className="relative grid grid-flow-dense custom h-full select-none p-4 bg-gradient-to-t from-pink-500 to-purple-500 text-white">
+                <img
+                  className="max-h-full pointer-events-none select-none rounded-lg shadow "
+                  src={item.image}
+                  alt="Image ${item.name}"
+                />
+                <h2 className="text-center">{item.name}</h2>
+                <p className="text-center">{item.description}</p>
+                <button className="flex justify-center items-center font-normal bg-white rounded-xl cursor-pointer mt-2 text-black">
+                  Wybieram wycieczkę
+                </button>
               </div>
             </SwiperSlide>
           ))}
